33989fde6e8d4dd9187cb250fa7d7fdb6c3c8d63
DanielW48 <31013771+DanielW48@users.noreply.github.com>
2018-11-15 13:40:08 -0500

    Create AreaOfPolygon.java

---
+++ AreaOfPolygon.java
@@ -0,0 +1,24 @@
+public class AreaOfPolygon {
+
+	public static void main(String[] args) {
+		
+		int n = 5;
+		
+		double[] x = new double[n], y = new double[n];
+		
+		double area = 0;
+		
+		for (int i = 0; i < n; i++) {
+			if (i == n-1) {
+				area += x[i] * y[0];
+			  area += y[i] * x[0];
+			}
+      else {
+				area += x[i] * y[i+1];
+				area += y[i] * x[i+1];
+			}
+		}
+		
+		area /= 2.0;
+	}
+}
