b139ee64053ab1251deac8055249ca5002c6dab3
DanielW48 <31013771+DanielW48@users.noreply.github.com>
2018-11-16 09:28:03 -0500

    Update BIT.java

--- BIT.java
+++ BIT.java
@@ -20,21 +20,20 @@ public class BIT{
     return out;
   }
   long getKth(int k) {
-		int lastK = tree.length;
+		int lastK = n;
 		int pos = 0;
-	  int pow2 = Integer.highestOneBit(lastK);
-		while (pow2 > 0) {
-			if (pos + pow2 <= tree.length) {
-				if (tree[pos+pow2] >= k) {
-					lastK = Math.min(lastK, pos+pow2);
+		int pow2 = Integer.highestOneBit(lastK);
+		while(pow2 > 0){
+			if(pos + pow2 <= n){
+				if(arr[pos + pow2] >= k){
+					lastK = Math.min(lastK, pos + pow2);
 				}
-        else {
+				else{
 					pos += pow2;
-					k -= tree[pos];
+					k -= arr[pos];
 				}
 			}
 			pow2 >>= 1;
 		}
 		return lastK;
-	}
 }
