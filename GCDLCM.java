7258d652062fbac3eff08286e9361796a450a86a
DanielW48 <31013771+DanielW48@users.noreply.github.com>
2019-02-11 10:00:24 -0500

    Update GCDLCM.java

--- GCDLCM.java
+++ GCDLCM.java
@@ -1,13 +1,13 @@
 public class GCDLCM {
-	public static int gcd(int a, int b) {
-		while (b> 0) {
-			int temp = b;
-			b = a%b;
+	public static long gcd(long a, long b) {
+		while(b > 0) {
+			long temp = b;
+			b = a % b;
 			a = temp;
 		}
 		return a;
 	}
-	public static int lcm(int a, int b) {
-		return a * (b / gcd(a,b));
+	public static long lcm(long a, long b) {
+		return a * (b / gcd(a, b));
 	}
 }
