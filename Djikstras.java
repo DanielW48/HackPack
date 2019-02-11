2c8e9c4223490f8750452d5f8d1484c13385a8b9
DanielW48 <31013771+DanielW48@users.noreply.github.com>
2019-02-11 09:58:20 -0500

    Update Djikstras.java

--- Djikstras.java
+++ Djikstras.java
@@ -1,30 +1,41 @@
+import java.util.*;
 public class Djikstras {
-	static int n;
-	static ArrayDeque[] edges;
-	static long[] d;
-	public static void main(String[] args) {
-		
-	}
-	static void solve(int start){
-		ArrayDeque<Integer> ad = new ArrayDeque<>();
-		ad.offer(start);
-		d[start] = 0;
-		while(!ad.isEmpty()){
-			int curr = ad.poll();
-			for(edge e : (ArrayDeque<edge>)edges[curr]){
-				long currd = d[curr] + e.w;
-				if(currd < d[e.v]){
-					d[e.v] = currd;
-					ad.offer(e.v);
-				}
-			}
-		}
+    static int n, s;
+    static ArrayDeque[] edges;
+    static long[] d;
+	public static void main (String[] args){
+	    PriorityQueue<state> pq = new PriorityQueue<>();
+	    d[s] = 0;
+	    pq.add(new state(s, 0));
+	    while(!pq.isEmpty()){
+	        state curr = pq.poll();
+	        if(curr.d > d[curr.i]) continue;
+	        
+	        for(edge e : (ArrayDeque<edge>) edges[curr.i]){
+	            long currd = d[curr.i] + e.w;
+	            if(currd < d[e.v]){
+	                d[e.v] = currd;
+	                pq.add(new state(e.v, currd));
+	            }
+	        }
+	    }
 	}
 	static class edge{
-		int v, w;
-		edge(int vv, int ww){
-			v = vv;
-			w = ww;
-		}
+	    int v, w;
+	    edge(int vv, int ww){
+	        v = vv;
+	        w = ww;
+	    }
+	}
+	static class state implements Comparable<state>{
+	    int i;
+	    long d;
+	    state(int ii, long dd){
+	        i = ii;
+	        d = dd;
+	    }
+	    public int compareTo(state in){
+	        return Long.compare(d, in.d);
+	    }
 	}
 }
