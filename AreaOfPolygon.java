1b3457d2533f800c00706344121692dc571462b6
DanielW48 <31013771+DanielW48@users.noreply.github.com>
2019-02-16 21:45:55 -0500

    Update AreaOfPolygon.java

--- AreaOfPolygon.java
+++ AreaOfPolygon.java
@@ -1,16 +1,13 @@
 class AreaOfPolygon {
 	public static void main(String[] args) {
-		int n = 5;
-		
-		double[] x = new double[n], y = new double[n];
-		
-		double area = 0;
-		
-		for (int i = 0; i < n; i++) {
-			area += x[i] * y[(i + 1) % n];
-			area += y[i] * x[(i + 1) % n];
-		}
-		
-		area /= 2;
+	    int n;
+	    double[] x = new double[n], y = new double[n];
+	    
+	    double area = 0;
+	    for (int i = 0; i < n; i++) {
+	        area += x[i] * y[(i + 1) % n];
+	        area += y[i] * x[(i + 1) % n];
+	    }
+	    area /= 2;
 	}
 }
