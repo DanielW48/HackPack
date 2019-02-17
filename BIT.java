44c806f4869898f606812a7daaf954e159a5fc6b
DanielW48 <31013771+DanielW48@users.noreply.github.com>
2019-02-16 21:50:00 -0500

    Update BIT.java

--- BIT.java
+++ BIT.java
@@ -1,39 +1,38 @@
-public class BIT{
-  int n;
-  long[] arr;
-  BIT(int nn){
-    n = nn;
-    arr = new long[n + 1];
-  }
-  void update(int idx, long v){
-    while(idx <= n){
-      arr[idx] += v;
-      idx += (idx & -idx);
+class BIT{
+    int n;
+    long[] arr;
+    BIT(int nn){
+        n = nn;
+        arr = new long[n + 1];
     }
-  }
-  long sum(int idx){
-    long out = 0;
-    while(idx > 0){
-      out += arr[idx];
-      idx -= (idx & -idx);
+    void update(int idx, long v){
+        while(idx <= n){
+            arr[idx] += v;
+            idx += (idx & -idx);
+        }
     }
-    return out;
-  }
-  long getKth(int k) {
-		int lastK = n;
-		int pos = 0;
-		int pow2 = Integer.highestOneBit(lastK);
-		while(pow2 > 0){
-			if(pos + pow2 <= n){
-				if(arr[pos + pow2] >= k){
-					lastK = Math.min(lastK, pos + pow2);
-				}
-				else{
-					pos += pow2;
-					k -= arr[pos];
-				}
-			}
-			pow2 >>= 1;
-		}
-		return lastK;
+    long sum(int idx){
+        long out = 0;
+        while(idx > 0){
+            out += arr[idx];
+            idx -= (idx & -idx);
+        }
+        return out;
+    }
+    long getKth(int k) {
+        int lastK = n;
+        int pos = 0;
+        int pow2 = Integer.highestOneBit(lastK);
+        while(pow2 > 0){
+            if(pos + pow2 <= n){
+                if(arr[pos + pow2] >= k) lastK = Math.min(lastK, pos + pow2);
+                else{
+                    pos += pow2;
+                    k -= arr[pos];
+                }
+            }
+            pow2 >>= 1;
+        }
+        return lastK;
+    }
 }
