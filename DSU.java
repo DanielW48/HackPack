ac03acc5ebbf08300ce1b78c5d79bcdad9a46e25
DanielW48 <31013771+DanielW48@users.noreply.github.com>
2019-02-16 22:02:38 -0500

    Update DSU.java

--- DSU.java
+++ DSU.java
@@ -5,36 +5,36 @@ class DSU {
 	    root = new int[n];
 	    height = new int[n];
 	    for(int i = 0; i < n; ++i) root[i] = i;
-	    w
+	    
 	    long sum = 0;
 	    int max = 0;
-        PriorityQueue<edge> pq = new PriorityQueue<>();
-        while(!pq.isEmpty()){
-            edge curr = pq.poll();
-            if(union(curr.u, curr.v)){
-                sum += curr.w;
-                max = curr.w;
-            }
-        }
+	    PriorityQueue<edge> pq = new PriorityQueue<>();
+	    while(!pq.isEmpty()){
+	        edge curr = pq.poll();
+	        if(union(curr.u, curr.v)){
+	            sum += curr.w;
+	            max = curr.w;
+	        }
+	    }
 	}
 	static int getPar(int idx) {
-		if(root[idx] != idx) return getPar(root[idx]);
-		return idx;
+	    if(root[idx] != idx) return getPar(root[idx]);
+	    return idx;
 	}
 	static boolean union(int x, int y) {
-		int parX = getPar(x);
-		int parY = getPar(y);
-		if(parX == parY) return false;
-		
-		if(height[parX] < height[parY]) {
-			root[parY] = parX;
-			height[parX] += height[parY]+1;
-		}
-		else {
-			root[parX] = parY;
-			height[parY] += height[parX]+1; 
-		}
-		return true;
+	    int parX = getPar(x);
+	    int parY = getPar(y);
+	    if(parX == parY) return false;
+	    
+	    if(height[parX] < height[parY]) {
+	        root[parY] = parX;
+	        height[parX] += height[parY]+1;
+	    }
+	    else {
+	        root[parX] = parY;
+	        height[parY] += height[parX]+1;
+	    }
+	    return true;
 	}
 	static class edge implements Comparable<edge>{
 	    int u, v, w;
