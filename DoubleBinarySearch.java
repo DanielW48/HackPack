25841e1c0eb8ebbc25ec56d9a755a8adc25ec655
DanielW48 <31013771+DanielW48@users.noreply.github.com>
2019-02-21 20:48:10 -0500

    Update DoubleBinarySearch.java

--- DoubleBinarySearch.java
+++ DoubleBinarySearch.java
@@ -1,19 +1,12 @@
 class DoubleBinarySearch {
 	public static void main (String[] args){
-	    double l = 0, h = 1e9 + 3, prev = -1;
-	    int count = 0;
-	    while(h - l >= 1e-8){
+	    double l = 0, h = 1e9 + 3;
+	    for(int i = 0; i < 200; ++i){
 	        double g = (l + h) / 2;
-	        
-	        if(g == prev){
-	            if(++count == 70) break;
-	        }
 	        
 	        //make sure to set bounds accordingly
 	        if(go(g)) h = g;
 	        else l = g;
-	        
-	        prev = g;
 	    }
 	    
 	    if(l > 1e9 + 1){
