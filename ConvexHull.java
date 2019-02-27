885de8478d3101aafb1805df220c59d7087276fa
DanielW48 <31013771+DanielW48@users.noreply.github.com>
2019-02-27 10:38:06 -0500

    Update ConvexHull.java

--- ConvexHull.java
+++ ConvexHull.java
@@ -1,59 +1,68 @@
 class ConvexHull {
     static int n;
     static point[] arr;
+    static point start;
     public static void main (String[] args){
         arr = new point[n];
         
-        int startI = -1;
         point start = arr[0];
-        for(int i = 0; i < n; ++i){
-            if(arr[i].x < start.x){
-                start = arr[i];
-                startI = i;
-            }
+        for(int i = 1; i < n; ++i){
+            if(arr[i].y < start.y || (arr[i].y == start.y && arr[i].x < start.x)) start = arr[i];
         }
+        
+        Arrays.sort(arr);
         
         ArrayDeque<point> stk = new ArrayDeque<>();
-        int ai = startI;
-        point a = start;
-        while(true){
-            int bi = 0;
-            point b = arr[0];
-            for(int i = 0; i < n; ++i){
-                if(i == ai || i == bi) continue;
-                
-                point c = arr[i];
-                int cp = crossProd(a, b, c);
-                long db = d(a, b), dc = d(a, c);
-                if(cp > 0 || (cp == 0 && dc > db)){
-                    stk.pop();
-                    stk.push(c);
-                    bi = i;
-                    b = c;
-                }
+        stk.push(arr[0]);
+        stk.push(arr[1]);
+        for(int i = 2; i < n; ++i){
+            point curr = arr[i];
+            point mid = stk.pop();
+            point prev = stk.pop();
+            
+            //change to <= to skip collinear points... doesn't always work?
+            while(crossProd(prev, mid, curr) < 0){
+                mid = prev;
+                prev = stk.pop();
             }
             
-            if(bi == startI){
-                stk.pop();
-                break;
-            }
-            
-            ai = bi;
-            a = b;
+            stk.push(prev);
+            stk.push(mid);
+            stk.push(curr);
+        }
+        
+        double perim = 0;
+        point prev = start;
+        for(point curr : stk){
+            perim += prev.d(curr);
+            prev = curr;
         }
         
     }
-    static long d(point a, point b){
-        return (long)(b.x - a.x) * (b.x - a.x) + (long)(b.y - a.y) * (b.y - a.y);
-    }
+    //if cross product > 0, then AC is to the left of AB
     static int crossProd(point a, point b, point c){
         return (a.x - b.x) * (a.y - c.y) - (a.x - c.x) * (a.y - b.y);
     }
-    static class point{
+    static class point implements Comparable<point> {
         int x, y;
         point(int xx, int yy){
             x = xx;
             y = yy;
+        }
+        double d(point in){
+            return Math.sqrt(Math.pow(x - in.x, 2) + Math.pow(y - in.y, 2));
+        }
+        public int compareTo(point in){
+            if(x == start.x && y == start.y) return -1;
+            if(in.x == start.x && in.y == start.y) return 1;
+            
+            int cp = crossProd(start, in, this);
+            if(cp != 0) return cp;
+            
+            return Double.compare(start.d(this), start.d(in));
+        }
+        public String toString(){
+            return x + " " + y;
         }
     }
 }
