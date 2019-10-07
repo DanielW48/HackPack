46c03f31729a2cf040ebbd2b978c9bc14a96bfbd
DanielW48 <31013771+DanielW48@users.noreply.github.com>
2019-10-07 11:56:56 -0400

    Create FordFulkerson.java

---
+++ FordFulkerson.java
@@ -0,0 +1,35 @@
+class FordFulkerson {
+	int n, s, t;
+	int[][] cap;
+	boolean[] seen;
+	FordFulkerson(int nn){
+		n = nn + 2;
+		s = n - 2;
+		t = n - 1;
+		cap = new int[n][n];
+	}
+	void addEdge(int u, int v, int c) {
+		cap[u][v] = c;
+	}
+	int ff() {
+		int out = 0;
+		while(true) {
+			seen = new boolean[n];
+			int add = dfs(s, Integer.MAX_VALUE);
+			if(add == 0) break;
+			out += add;
+		}
+		return out;
+	}
+	int dfs(int idx, int min) {
+		seen[idx] = true;
+		if(idx == t) return min;
+		for(int i = 0; i < n; ++i) {
+			if(!seen[i] && cap[idx][i] > 0) {
+				int a = dfs(idx, Math.min(min, cap[idx][i]));
+				if(a > 0) return a;
+			}
+		}
+		return 0;
+	}
+}
