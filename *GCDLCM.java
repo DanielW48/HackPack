1e2492e3e68b452b07c5d9d39fb188c8271f761f
DanielW48 <31013771+DanielW48@users.noreply.github.com>
2021-04-08 23:03:59 -0400

    Update and rename GCDLCM.java to *GCDLCM.java

---
+++ *GCDLCM.java
@@ -0,0 +1,18 @@
+class GCDLCM {
+	static long gcd(long a, long b){
+		if(b > a){
+			int temp = b;
+			b = a;
+			a = temp;
+		}
+		while(b > 0){
+			int temp = a%b;
+			a = b;
+			b = temp;
+		}
+		return a;
+	}
+	static long lcm(long a, long b) {
+		return a / gcd(a, b) * b;
+	}
+}
--- GCDLCM.java
+++
@@ -1,8 +0,0 @@
-class GCDLCM {
-    static long gcd(long a, long b) {
-        return b == 0 ? a : gcd(b, a % b);
-    }
-    static long lcm(long a, long b) {
-        return a / gcd(a, b) * b;
-    }
-}
